bd2b59163c22a3ab148f3d8eaaccd013edcf7ac6
Jocker yu <f38988483@gmail.com>
2024-01-05 21:15:06 +0800

    Project init

---
+++ xdclass-wechat-pay-learning/src/main/java/net/xdclass/xdclasswechatpaylearning/XdclassWechatPayLearningApplication.java
@@ -0,0 +1,13 @@
+package net.xdclass.xdclasswechatpaylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdclassWechatPayLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdclassWechatPayLearningApplication.class, args);
+	}
+
+}
---
+++ xdclass-wechat-pay-learning/src/test/java/net/xdclass/xdclasswechatpaylearning/XdclassWechatPayLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xdclass.xdclasswechatpaylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdclassWechatPayLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
